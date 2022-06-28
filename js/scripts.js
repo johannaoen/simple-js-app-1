e75946858fba47fc7eda40c2a10888e70f13fca4
johannaoen <107009997+johannaoen@users.noreply.github.com>
2022-06-28 15:34:27 -0700

    Added pokemon names/height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
-let pokemonList = {
-  name:"Bulbasaur",
-  height:7,
+let pokemonList = [
+  {name:"Bulbasaur",
+  height: 0.7,
   types:['grass', 'poison', 'fighting', 'steel']
-};
+},
+{name: "Charizard",
+height: 1.7,
+types: ["fire", "flying"]
+},
+{
+  name: "Squirtle",
+  height: 1,
+  types: ["water"]
+}
+]
+;
+for(let i=0; i<pokemonList.length; i++){
+document.write (pokemonList[i].name + pokemonList[i].height + "<p>");
+}
+let result;
+if(height>1) {
+  result = "That is a BIG pokemon";
+}
