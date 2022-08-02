167d61dad34d036411b5bb3f40fc58c10af0f585
johannaoen <107009997+johannaoen@users.noreply.github.com>
2022-08-01 17:22:19 -0700

    adding dialogs

--- js/scripts.js
+++ js/scripts.js
@@ -85,4 +85,104 @@ pokemonRepository.loadList().then(function() {
 });
 
 //Task1.8
-//shows one error message at a time
+
+let pokemonRepository = (function() {
+  let modalContainer = document.querySelector('#modal-container');
+
+  function showModal(title, text) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+  let dialogPromiseReject;
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+//Task 1.9
+function showDialog(title, text) {
+  showModal(title, text);
+  let modal = modalContainer.querySelector('.modal');
+  //We have defined modalContainer here
+
+  //We want to add a confirm and cancel button to the modal
+let confirmButton = document.createElement('button');
+confirmButton.classList.add('modal-confirm');
+confirmButton.innerText = 'Confirm';
+let cancelButton = document.createElement('button');
+cancelButton.classList.add('modal-cancel');
+cancelButton.innerText = 'Cancel';
+
+modal.appendChild(confirmButton);
+modal.appendChild(cancelButton);
+//We want to focus the confirmButton so that the user can simply press enter
+confirmButton.focus();
+
+return new Promise((resolve, reject) =>
+{
+  cancelButton.addEventListener('click', hideModal);
+  confirmButton.addEventListener('click', () => {
+    dialogPromiseReject = null;
+    hideModal();
+    resolve();
+});
+dialogPromiseReject = reject;
+});
+}
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Modal title', 'This is the modal content!');
+});
+
+// THE RETURN STATEMENT HERE
+})();
+
+document.querySelector('#show-dialog').addEventListener('click', () => {
+showDialog('Confirm action', 'Are you sure you want to do this?').then(function(){
+alert('confirmed!');
+}, () => {
+alert('not confirmed');
+});
+});
+//End of Task 1.9
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  document.querySelector('#show-modal').addEventListener('click', () => {
+      showModal('Modal title', 'This is the modal content!');
+    });
+
+  })();
